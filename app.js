e6dcdd8c6e761681e868ade6c282f1101733ca57
alouzizi <alouzizi@student.1337.ma>
2023-10-02 12:21:33 +0100

    Deleting Todos

--- app.js
+++ app.js
@@ -20,3 +20,11 @@ addForm.addEventListener('submit', e => {
 		addForm.reset();
 	}
 });
+
+// Delete Todos
+
+list.addEventListener('click', e => {
+	if(e.target.classList.contains('delete')){
+		e.target.parentElement.remove();
+	}
+});
