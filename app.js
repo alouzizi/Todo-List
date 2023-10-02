127eede785e0a0245aa0e9478feca98f14405eb5
alouzizi <alouzizi@student.1337.ma>
2023-10-02 12:51:10 +0100

    Sesrching & Filtering Todos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
 const generateTemplate = todo => {
 	const html = `
@@ -27,4 +28,22 @@ list.addEventListener('click', e => {
 	if(e.target.classList.contains('delete')){
 		e.target.parentElement.remove();
 	}
+});
+
+// keyup event
+
+const filterTodos = (term) => {
+
+	Array.from(list.children)
+	.filter(todo => !todo.textContent.toLowerCase().includes(term))
+	.forEach(todo => todo.classList.add('filtred'));
+
+	Array.from(list.children)
+	.filter(todo => todo.textContent.toLowerCase().includes(term))
+	.forEach(todo => todo.classList.remove('filtred'));
+};
+
+search.addEventListener('keyup', () => {
+	const term = search.value.trim().toLowerCase();
+	filterTodos(term);
 });
